846211f52173bf7b72761d0b0afb64654d4a4681
alexparfeno <75564023+alexparfeno@users.noreply.github.com>
2020-12-11 19:39:44 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,18 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+from math import sqrt
+
+numbers2=[]
+
+numbers=[2, -5, 8, 9, -25, 25, 4]
+
+for number in numbers:
+
+    if number>=0 and sqrt(number)%1 == 0:
+
+        numbers2.append(int(sqrt(number)))
+
+       
+
+print(numbers2)
